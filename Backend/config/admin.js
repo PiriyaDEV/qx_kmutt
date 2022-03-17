1efcf33086fbbfa3c4b25553627b662eaebfcec5
Phumiphat <harryphat42@gmail.com>
2022-03-18 00:09:30 +0700

    Updated configuration files

--- Backend/config/admin.js
+++ Backend/config/admin.js
@@ -1,5 +1,6 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', '53748b09ba05ec0033f57b6c6ff66b72'),
+    secret: env("ADMIN_JWT_SECRET", "53748b09ba05ec0033f57b6c6ff66b72"),
   },
+  //url: env("DASHBOARD_URL", "http://localhost:1337/dashboard"),
 });
